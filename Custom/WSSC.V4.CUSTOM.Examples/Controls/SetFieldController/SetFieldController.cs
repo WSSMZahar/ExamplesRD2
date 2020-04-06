4c8ba9a3d536c230e2adaa1dc9551ce550f2e441
WSSMZahar <zmazyrin@wss-consulting.ru>
2020-04-06 16:23:51 +0500

    Добавил файлы

---
+++ Custom/WSSC.V4.CUSTOM.Examples/Controls/SetFieldController/SetFieldController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WSSC.V4.CUSTOM.Examples;
+using WSSC.V4.SYS.DBFramework;
+
+namespace WSSC.V4.DMS.FOS
+{
+    /// <summary>
+    /// Простановка значения поля "На контроле" по значению поля "Контролер"
+    /// </summary>
+    public class SetFieldController : DBListFormWebControl
+    {
+        protected SetFieldController(DBListFormWebControlMetadata metadata, DBListFormControl listForm)
+            : base(metadata, listForm) { }
+
+        protected class Factory : DBListFormWebControlFactory
+        {
+            protected override DBListFormWebControl CreateListFormWebControl(DBListFormWebControlMetadata metadata, DBListFormControl listForm)
+            {
+                return new SetFieldController(metadata, listForm);
+            }
+        }
+
+        /// <summary>
+        /// Добавление обработчиков на поля
+        /// </summary>
+        protected override void OnListFormInitCompleted()
+        {
+            this.AppContext.ScriptManager.RegisterResource("Controls/SetFieldController/SetFieldController.js", VersionProvider.ModulePath);
+            this.AddFieldChangeHandler(Consts.SetFieldControllerHandler.ControllerFieldName, "FOS_SetFieldController");            
+        }
+
+        /// <summary>
+        /// Событие открытия формы
+        /// </summary>
+        protected override string ClientInitHandler
+        {
+            get
+            {
+                return "FOS_SetFieldController_Init";
+            }
+        }
+    }
+}
---
+++ Custom/WSSC.V4.CUSTOM.Examples/Handlers/SetFieldControllerHandler/SetFieldControllerHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WSSC.V4.SYS.DBFramework;
+using WSSC.V4.SYS.Fields.Lookup;
+using WSSC.V4.CUSTOM.Examples;
+
+namespace WSSC.V4.DMS.FOS
+{
+    public class SetFieldControllerHandler : DBItemHandler
+    {
+        public SetFieldControllerHandler(DBItemHandlerDefinition handlerDefinition, DBList list)
+            : base(handlerDefinition, list) { }
+
+    
+        protected override void OnBeforeItemUpdate(DBItemOperation operationProperties)
+        {
+            if (operationProperties == null)
+                return;                             
+
+            DBItem item = operationProperties.Item;
+            if (item == null)
+                return;
+
+            //только при создании
+            if (!item.ContextCreated)
+                return;
+
+            bool onController = item.GetValue<bool>(Consts.SetFieldControllerHandler.OnСontrollerFieldName);
+            DBFieldLookupValue controller = item.GetLookupValue(Consts.SetFieldControllerHandler.ControllerFieldName);
+            bool value = controller == null ? false : true;
+
+            //Если "Контролер" пусто и  "На контроле" false заново ставить false не нужно, с true аналогично
+            if (!value && !onController || (value && onController))
+                return;
+
+            item.SetValue(Consts.SetFieldControllerHandler.OnСontrollerFieldName, value);
+           
+        }
+    }
+}
