2206936488d8a4a729ff81b6b242d475671ea365
WSSMZahar <zmazyrin@wss-consulting.ru>
2020-04-13 12:41:40 +0500

    Правки CR.

---
+++ Custom/WSSC.V4.CUSTOM.Examples/Controls/SetFieldController/SetFieldController.js
@@ -0,0 +1,18 @@
+FOS_ControllerFieldName = "Контролер";
+FOS_OnControlFieldName = "На контроле";
+function FOS_SetFieldController() {
+    var controller = ListForm.GetField(FOS_ControllerFieldName, true);
+    if (controller == null)
+        return;
+    var onControl = ListForm.GetField(FOS_OnControlFieldName, true);
+    if (onControl == null)
+        return;
+    var controllerValue = controller.GetValue();
+    //если контролер не проставлен то "На контроле" ставим "Нет", иначе "Да"
+    var value = controllerValue == null || controllerValue.LookupID != 0;
+    onControl.SetValue(value);
+}
+function FOS_SetFieldController_Init() {
+    FOS_SetFieldController();
+}
+//# sourceMappingURL=SetFieldController.js.map
